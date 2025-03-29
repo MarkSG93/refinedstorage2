4c9db9ac7242531b0ec69e4c470642decea22526
Starforcraft <starforcraft@yahoo.com>
2025-03-29 16:49:10 +0100

    fix: addons registering their own upgrade mappings results in a ConcurrentModificationException

--- refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/upgrade/UpgradeRegistryImpl.java
+++ refinedstorage-common/src/main/java/com/refinedmods/refinedstorage/common/upgrade/UpgradeRegistryImpl.java
@@ -5,17 +5,17 @@ import com.refinedmods.refinedstorage.common.api.upgrade.UpgradeMapping;
 import com.refinedmods.refinedstorage.common.api.upgrade.UpgradeRegistry;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class UpgradeRegistryImpl implements UpgradeRegistry {
-    private final Map<UpgradeDestination, Set<UpgradeMapping>> byDestination = new HashMap<>();
-    private final Map<Item, Set<UpgradeMapping>> byUpgradeItem = new HashMap<>();
+    private final Map<UpgradeDestination, Set<UpgradeMapping>> byDestination = new ConcurrentHashMap<>();
+    private final Map<Item, Set<UpgradeMapping>> byUpgradeItem = new ConcurrentHashMap<>();
 
     @Override
     public DestinationBuilder forDestination(final UpgradeDestination destination) {
@@ -23,8 +23,10 @@ public class UpgradeRegistryImpl implements UpgradeRegistry {
             @Override
             public DestinationBuilder add(final Item upgradeItem, final int maxAmount) {
                 final UpgradeMapping mapping = createMapping(destination, upgradeItem, maxAmount);
-                byDestination.computeIfAbsent(destination, key -> new LinkedHashSet<>()).add(mapping);
-                byUpgradeItem.computeIfAbsent(upgradeItem, key -> new LinkedHashSet<>()).add(mapping);
+                byDestination.computeIfAbsent(destination, key ->
+                    Collections.synchronizedSet(new LinkedHashSet<>())).add(mapping);
+                byUpgradeItem.computeIfAbsent(upgradeItem, key ->
+                    Collections.synchronizedSet(new LinkedHashSet<>())).add(mapping);
                 return this;
             }
         };
